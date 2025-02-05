a76428bf08638792778d63605b6f9d2297db2f9d
Dhaneshwar Kumar <dhannesh@gmail.com>
2025-02-05 21:22:01 +0530

    floating action button add

--- README.md
+++ README.md
@@ -2,15 +2,5 @@
 
 A new Flutter project.
 
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Scaffold :
+occupies all of the devices screen spaces and provides the basis material design layout
--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,6 @@ void main() {
   runApp(const MyStore());
 }
 
-class SimpleApp extends StatelessWidget {
-  const SimpleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: "Pratham Shree!",
-        home: Scaffold(
-            body: Center(
-                child: Text("Welcome to Insta Store",
-                    style: TextStyle(fontSize: 24, color: Colors.blue),
-                    textDirection: TextDirection.ltr))));
-  }
-}
-
 class MyStore extends StatelessWidget {
   const MyStore({super.key});
   @override
@@ -26,9 +11,7 @@ class MyStore extends StatelessWidget {
     return MaterialApp(
         title: 'Insta Store',
         home: Scaffold(
-          backgroundColor: Colors.teal,
           appBar: AppBar(
-            backgroundColor: Colors.redAccent,
             title: const Text('Insta Store 2'),
           ),
           body: Column(
@@ -40,13 +23,7 @@ class MyStore extends StatelessWidget {
                 margin: const EdgeInsets.all(30),
                 padding: const EdgeInsets.all(40),
                 decoration: const BoxDecoration(
-                  color: Colors.white70,
-                  shape: BoxShape.circle,
 
-                  // gradient: LinearGradient(
-                  //     begin: Alignment.topCenter,
-                  //     end: Alignment.bottomCenter,
-                  //     colors: [Colors.yellow,Colors.red])
                 ),
               // child: const FlutterLogo(),
                 child: Image.network("https://openclipart.org/image/800px/89299"),
@@ -59,11 +36,15 @@ class MyStore extends StatelessWidget {
                             fontSize: 24,
                             fontFamily: 'Verdana',
                             // color: Color.fromARGB(200, 250, 150, 214),
-                            color:Colors.red,
                             fontWeight: FontWeight.bold)),
                   ),
                 ),
             ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {debugPrint('Thank you for liking us!');},
+            tooltip: 'Like Us!',
+            child: const Icon(Icons.favorite),
           ),
         ));
   }
